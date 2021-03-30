f42639ba3f4fd155d4f0c512a97f69714355031e
Voronov Vladimir <vova.voronov.2002@bk.ru>
2021-03-30 11:47:27 +0300

    Version of the calculator with vectors

--- calculator/src/calculator.c
+++ calculator/src/calculator.c
@@ -1,6 +1,6 @@
 /*Name        : calculator.c
  Author      : Vladimir Voronov
- Version     : 1.1
+ Version     : 1.5
  Copyright   : Ivt
  Description : calculator
  Git	     : Вы должны обладать ссылкой (https://github.com/SunnBr0/test_01).
@@ -12,70 +12,116 @@
 Жмем finish и видим, что появилась папка в git repositories заходим в эту папку, дальше Working tree.
 Видим папку calculator, жмем 1 раз на нее, потом правой кнопкой. И жмем Import Project.
 Появляется окно, в нем ничего выбирать не нужно, просто если Eclipse выбрал не ту папку, то стоит ее редактировать.
-Жмем finish и уже проект появляется в C/C++. Теперь вы можете смотреть код (calculator/src/calculator.c)
+Жмем finish и уже проект появляется в C/C++. Теперь вы можете смотрет/ код(calculator/src/calculator.c)
 
 */
 #include <stdio.h>
+#include <stdlib.h>
 
 int main() {
 	setvbuf(stdout, NULL, _IONBF, 0);
 	setvbuf(stderr, NULL, _IONBF, 0);
+	float *onevector,*twovector;
+	int size;
 	float a, b, res, stepen;
+	int vbor;
+	float result = 0.0;
 	char operation;
 	char next;
+	printf("вы хотите работать с векторами ");
 	printf("Hello , i am cancilator\n");
 	printf("My operation:\n");
 	printf("+- вы можете plus два числа ( 1 + 2 ) или minus ( 3 - 2 )\n");
-	printf("* или / вы можете умножить ( 1 * 3 ) или поделить ( 1 / 2 )\n ");	printf(
-			"^ вы можете возвести в степень даже в отрицательную ( 2 ^ 3 ) , ( 5 ^ -2 )\n");
+	printf("* или / вы можете умножить ( 1 * 3 ) или поделить ( 1 / 2 )\n ");
+	printf("^ вы можете возвести в степень даже в отрицательную ( 2 ^ 3 ) , ( 5 ^ -2 )\n");
 	printf("! вы можете найти факториал числа ( 5 ! )\n\n");
 	do {
-		scanf("%f %c", &a, &operation);
-		if (operation != '!') {	                     // тут необходимое условие тк для факториала нужно только ввести число и операцию
-			scanf("%f", &b);
-			switch (operation) {                     //тут происходит выбор операции над числом или числами
-			case ('+'):
-				printf("%f %c %f = %f\n", a, operation, b, a + b);
+		printf("С чем будете работать с числами или векторами (0 - числа, 1-векторами)\n"); // выбор с чем работать с числами или с векторами
+		scanf("%i",&vbor);
+		if(vbor == 1){
+			printf("Введите размер вектора:\n");
+			scanf("%i",&size);
+			onevector = malloc(size*sizeof(int));
+			twovector = malloc(size*sizeof(int));
+			printf("Введите вектор один:\n");
+			for(int i = 0; i < size ;i++){scanf("%f",&onevector[i]);} // заполняем первый вектор
+
+			printf("Введите вектор два:\n");
+			for(int i = 0; i < size ;i++){scanf("%f",&twovector[i]);} // заполняем второй вектор
+
+			printf("Введите операцию над векторами(+ - *):\n");
+
+			scanf(" %c", &operation);
+			switch(operation){		// операции с векторами
+			case('+'):
+				for(int i = 0; i < size ;i++)printf("%f ",onevector[i] + twovector[i]);
 				break;
-			case ('-'):
-				printf("%f %c %f = %f\n", a, operation, b, a - b);
+			case('-'):
+				for(int i = 0; i < size ;i++)printf("%f ",onevector[i] - twovector[i] );
 				break;
-			case ('/'):
-				printf("%f %c %f = %f\n", a, operation, b, a / b);
-				break;
-			case ('*'):
-				printf("%f %c %f = %f\n", a, operation, b, a * b);
-				break;
-			case ('^'):			                     // помимо простых действий над число ( + - * /),
-				stepen = 1;	                         //где нужно ничего особенно пиcать для реализации
-				if (b >= 0) {                        // в отличие от возведения в степень,где для реализации нужен цикл for
-					for (int i = 1; i <= b; i++) {
-						stepen *= a;
-					}
-					printf("%f %c %f = %f\n", a, operation, b, stepen);
-				} else if (b < 0) {
-					for (int i = 0; i > b; i--) {
-						stepen *= a;
-					}
-					printf("%f %c %f = %f\n", a, operation, b, 1 / stepen);
+			case('*'):
+				for(int i = 0; i < size ;i++){
+					result += onevector[i] * twovector[i];
 				}
+				printf("%f \n",result);
+				result = 0;
 				break;
-			default:				                 // если операция не определена то всё смэрть
-				printf("Error\n");
+			default:				                 // если операция не определена то будет ошибка
+					printf("Error\n");
 			}
-		} else {
-			res = 1;
-			if (a >= 0 && (a * 3 < (int)a)) {
-				for (int i = 1; i <= a; i++) {       // факториал реализован через цикл for
-					res *= i;
+			free(onevector);
+			free(twovector);
+			printf("Continue (y/n)? \n");				 // y - yes , n - not
+			scanf(" %c", &next);
+		}else{
+
+			scanf("%f %c", &a, &operation);
+			if (operation != '!') {	                     // тут необходимое условие тк для факториала нужно только ввести число и операцию
+				scanf("%f", &b);
+				switch (operation) {                     //тут происходит выбор операции над числом или числами
+				case ('+'):
+					printf("%f %c %f = %f\n", a, operation, b, a + b);
+					break;
+				case ('-'):
+					printf("%f %c %f = %f\n", a, operation, b, a - b);
+					break;
+				case ('/'):
+					printf("%f %c %f = %f\n", a, operation, b, a / b);
+					break;
+				case ('*'):
+					printf("%f %c %f = %f\n", a, operation, b, a * b);
+					break;
+				case ('^'):			                     // помимо простых действий над число ( + - * /),
+					stepen = 1;	                         //где нужно ничего особенно пиcать для реализации
+					if (b >= 0) {                        // в отличие от возведения в степень,где для реализации нужен цикл for
+						for (int i = 1; i <= b; i++) {
+							stepen *= a;
+						}
+						printf("%f %c %f = %f\n", a, operation, b, stepen);
+					} else if (b < 0) {
+						for (int i = 0; i > b; i--) {
+							stepen *= a;
+						}
+						printf("%f %c %f = %f\n", a, operation, b, 1 / stepen);
+					}
+					break;
+				default:				                 // если операция не определена то будет ошибка
+					printf("Error\n");
 				}
-				printf("%f %c = %f\n", a, operation, res);
-			} else {								 // обработка ошибки
-				printf("Error\n");
+			} else {
+				res = 1;
+				if (a >= 0 && (a * 3 < (int)a)) {
+					for (int i = 1; i <= a; i++) {       // факториал реализован через цикл for
+						res *= i;
+					}
+					printf("%f %c = %f\n", a, operation, res);
+				} else {								 // обработка ошибки
+					printf("Error\n");
+				}
 			}
+			printf("Continue (y/n)? \n");				 // y - yes , n - not
+			scanf(" %c", &next);
 		}
-		printf("Continue (y/n)? \n");				 // y - yes , n - not
-		scanf(" %c", &next);
 	} while (next == 'y');							 // цикл для того чтобы пользователь продолжил считать либо закончил работу
 	return 0;
 
