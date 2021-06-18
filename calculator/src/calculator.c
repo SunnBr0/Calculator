f90ee7249f94c3268dbde1d4a48c8595a454f105
Voronov Vladimir <vova.voronov.2002@bk.ru>
2021-06-18 16:24:36 +0300

    version 5.0. Implemented on a stack basis

--- calculator/src/calculator.c
+++ calculator/src/calculator.c
@@ -1,284 +1,141 @@
 /*Name        : calculator.c
  Author      : Vladimir Voronov
  Version     : 4
- Copyright   : VoronovVladimirIVT.VSU
+ Copyright   : IVT's group
  Description : calculator
  Git	     : Вы должны обладать ссылкой (https://github.com/SunnBr0/calculator).
 Калькулятор поддерживает работу с числами и с векторами,
-операция для чисел(+,*,-,/,^,!) операции для векторов (+,-,*(скалярное умножение))
+операция для чисел(+,*,-,/,^,!)
  Калькулятор может работать с входными файлами и также выводить результат в файл
 Обновление:
-Теперь метод хранение используется очередь.Принцип работы: первый зашёл первый вышел.
-То есть забирать элементы из очереди мы будем в том же порядке, что и клали
+ В принципе работы лежит структура stack , у которого принцип работы заключается в том,что
+ "первый зашёл ,последний вышел" либо "последний вошёл ,первый вышел"
+Теперь выражение обязательно нужно записывать в виде обратной польской аннотации
 Пример:
 Назовём файл input.txt ,запишем туда эти выражения:
-s - алгебраический калькулятор
-v - векторный калькулятор
-s + 1.1 5.9
-s - 4.25 0.25
-s / 2.5 1.25
-s * 5.2 2
-s ^ 2.1 6
-v + 3 1 4 1 2 2 3
-v - 3 1 4 1 2 1 2
-v * 3 1 4 1 2 2 2
+2 1 + 7 * 2 - n
+3 6 + 4 - 4 * n
+4 6 * 15 - 24 + n
 вывод пусть будет output.txt
 результат увидете сами
 */
 #include <stdio.h>
 #include <stdlib.h>
 
-// структура для элемента очереди
-typedef struct data {
-    char *elem;
-    struct data *next;
-} data;
-// струкрута, создаваемая для очереди
-typedef struct queue_t {
-    data *head;
-    data *tail;
-} queue_t;
-// инициализация пустой очереди
-queue_t init_queue() {
-    queue_t queue; // создаём саму очередь
-    queue.head = NULL;
-    queue.tail = NULL;
-    return queue;
-}
-// добавление элемента в конец очереди
-void enqueue(queue_t *queue, char *value) {
-    data *node = (data*) malloc(sizeof(data));
-    node->elem = value;
+typedef struct element {
+    float data;
+    struct element *next;
+} stack;
+stack *headStack = NULL;
 
-    node->next = NULL;
-    if (queue->tail == NULL) {
-        queue->head = node;
-    } else {
-        queue->tail->next = node;
-    }
-    queue->tail = node;
-}
-// удаление элемента из очереди
-char* dequeue(queue_t *queue) {
-    char *elem = queue->head->elem;
-
-    data *tmp = queue->head;
-    queue->head = queue->head->next;
-    free(tmp);
-    if (queue->head == NULL) {
-        queue->tail = NULL;
-    }
-    return elem;
-}
-// проверка очереди на пустоту
-int is_empty(queue_t queue) {
-    return queue.head == NULL;
-}
-// считывание строки из файла
-char* read_string(FILE *input) {
-    int size = 0; // размер строки
-    int len_size = 1;  // ёмкость строки
-    char *string = (char*) malloc(len_size * sizeof(char));
-    for (char c = fgetc(input); !feof(input) && c != '\n'; c = fgetc(input)) {
-        string[size++] = c;
-        if (size >= len_size) { // если ёмкости недостаточно
-            len_size *= 2;      // увеличиваем её в 2 раза
-            string = (char*) realloc(string, len_size * sizeof(char)); // и перераспределяем память
-        }
-    }
-    string[size] = '\0';
-    return string;
-}
-// проверка на цифру
-int is_digit(float digit) {
-    return (digit >= '0' && digit <= '9');
-}
-// ввод числа из строки
-int read_int(char *string, int *i) {
-    int value = 0;
-    while (string[*i] == ' ') {
-        (*i)++;
-    }
-    while (string[*i] && is_digit(string[*i])) {
-        value = value * 10 + string[(*i)++] - '0';
-    }
-    return value;
-}
-double read_double(char *string, int *i) {
-    int start = *i;
-    char prev = string[*i];
-    while (string[*i] == ' ') {
-        (*i)++;
-    }
-    while (string[*i] && (is_digit(string[*i]) || string[*i] == '.')) {
-        (*i)++;
-    }
-    string[*i] = '\0';
-    double value = atof(string + start); // переводим строку в число
-    string[*i] = prev;
-    return value;
-}
-double factorial(int number) {
-    double factorial = 1;
-    for (int i = 2; i <= number; i++) {
-        factorial *= i;
-    }
-    return factorial;
-}
-void numbers(FILE *output, char *string, char sign) {
-    fprintf(output,"What will you work with with numbers or vectors?  (s - numbers, v-vectors)\n"); // выбор с чем работать с числами или с векторами
-    fprintf(output,"s\n");
-    int index = 3;
-    if (sign == '!') {
-        int x = read_int(string, &index);
-        fprintf(output, "%.0lf\n", factorial(x));
-        return;
-    }
-    double x = read_double(string, &index);
-    double y = read_double(string, &index);
-    if (sign == '+') {
-        fprintf(output, "%.2lf %c %.2lf = %.2lf\n\n", x,sign,y,x + y);
-    } else if (sign == '-') {
-        fprintf(output, "%.2lf %c %.2lf = %.2lf\n\n", x,sign,y,x - y);
-    } else if (sign == '*') {
-        fprintf(output, "%.2lf %c %.2lf = %.2lf\n\n", x,sign,y,x * y);
-    } else if (sign == '/') {
-        if (y == 0)
-            fprintf(output, "division by zero\n");
-        else
-            fprintf(output, "%.2lf %c %.2lf = %.2lf\n\n", x,sign,y,(double)x / y);
-    } else if (sign == '^') {
-        float stepen = 1;
-        for (int i = 1; i <= y; i++) {
-            stepen *= x;
-        }
-        fprintf(output, "%.2lf %c %.2lf = %.2lf\n\n", x,sign,y,stepen);
-    } else {
-        fprintf(output, "unknown operation '%c'\n", sign);
-    }
-}
-void vectors(FILE *output, char *string, char sign) {
-    fprintf(output,"What will you work with with numbers or vectors?  (s - numbers, v-vectors)\n"); // выбор с чем работать с числами или с векторами
-    fprintf(output,"v\n");
-    int index = 3;
-    int size = read_int(string, &index); // размерность вектора
-    fprintf(output,"Enter the size of the vector:\n");
-    fprintf(output,"%i\n",size);
-    double *onevector = (double*) malloc(size * sizeof(double));
-    double *twovector= (double*) malloc(size * sizeof(double));
-    // считываем вектора
-    fprintf(output,"Enter vector one:\n");
-    for (int i = 0; i < size; i++){
-        onevector[i] = read_double(string, &index);
-        fprintf(output,"%f\n",onevector[i]);
-    }
-    fprintf(output,"Enter vector two:\n");
-    for (int i = 0; i < size; i++){
-        twovector[i] = read_double(string, &index);
-        fprintf(output,"%f\n",twovector[i]);
-    }
-    fprintf(output,"Enter the operation on the vectors(+ - *):\n");
-    fprintf(output,"%c\n",sign);
-    if (sign == '*') {
-        double res = 0;
-        fprintf(output, "( ");
-        for (int i = 0; i < size; i++)
-            fprintf(output, "%.2f ", onevector[i]);
-        fprintf(output, ") * ( ");
-        for (int i = 0; i < size; i++)
-            fprintf(output, "%.2f ", twovector[i]);
-        fprintf(output, ") = ");
-        for (int i = 0; i < size; ++i) {
-            if (i != size - 1 ){fprintf(output, "%.2f ", onevector[i]*twovector[i]);
-                fprintf(output,"+ ");
+float numberFunction(char operation, float number1, float number2){     //  function for working with numbers
+    float factorial = 1, stepen = 1, resultNumber;
+    switch (operation){  //  operation fetch block
+        case '+':
+            resultNumber = number1 + number2;
+            return resultNumber;  //  результат сложение
+        case '-':
+            resultNumber = number1 - number2;
+            return resultNumber;  //  результат вычитание
+        case '*':
+            resultNumber = number1 * number2;
+            return resultNumber;  // результат умножение
+        case '/':
+            if (number2 != 0){
+                resultNumber = number1 / number2;
+                return resultNumber;  //  результат деления
             }
             else{
-                fprintf(output, "%.2f ", onevector[i]*twovector[i]);
+                return 0;
             }
-        }
-        fprintf(output, " = ");
-        for (int i = 0; i < size; i++)
-            res += onevector[i] * twovector[i];
-        fprintf(output, "%.2f ", res);
-        fprintf(output,"\n\n");
-    } else if (sign == '+' || sign == '-') {
-        if (sign == '+') {
-            fprintf(output, "(");
-            for (int i = 0; i < size; i++) {
-                fprintf(output, " %.2f", onevector[i]);
+        case '!':
+            for (int i=1; i <= number1; i++){
+                factorial = factorial*i;
             }
-            fprintf(output, ") %c (",sign);
-            for (int i = 0; i < size; i++) {
-                fprintf(output, " %.2f", twovector[i]);
+            resultNumber = factorial;
+            return resultNumber;  //  результат факториала
+        case '^':
+            for (int i=1; i <= number2; i++){
+                stepen = stepen*number1;
             }
-            fprintf(output, " ) = ");
-            for (int i = 0; i < size; i++){
-                fprintf(output, "%.2lf ", onevector[i] + twovector[i]);
-            }
-            fprintf(output, "\n\n");
-        } else {
-            fprintf(output, "(");
-            for (int i = 0; i < size; i++) {
-                fprintf(output, " %.2f", onevector[i]);
-            }
-            fprintf(output, ") %c (",sign);
-            for (int i = 0; i < size; i++) {
-                fprintf(output, " %.2f", twovector[i]);
-            }
-            fprintf(output, " ) = ");
-            for (int i = 0; i < size; i++){
-                fprintf(output, "%.2lf ", onevector[i] - twovector[i]);
-            }
-            fprintf(output, "\n\n");
-        }
-    } else {
-        fprintf(output, "unknown operation '%c'\n", sign);
+            resultNumber = stepen;
+            return resultNumber;  //  результат степени
     }
-    free(onevector);
-    free(twovector);
+    return number1;
 }
-// вычисление значения выражения
-void calc(FILE *input, char *string) {
-    char vbor = string[0];  // выбор калькулятора(алгебраический или векторный)
-    char sign = string[2];
-    if (vbor == 's') {
-        numbers(input, string, sign);
-    } else if (vbor == 'v') {
-        vectors(input, string, sign);
-    }
+
+void pushElement(stack **head, float data){         //добавить элемент в стек структуру
+    stack *pushLocal = malloc(sizeof(stack));
+    pushLocal->next = *head;
+    pushLocal->data = data;
+    *head = pushLocal;
 }
-int main() {
+
+float popElement(stack **head){             //  remove element from stack`s struct
+    stack *popLocal;
+    float data;
+    popLocal = *head;
+    *head = (*head)->next;
+    data = popLocal->data;
+    return data;
+}
+
+int main(int argc, char *argv[]){
     setvbuf(stdout, NULL, _IONBF, 0);
     setvbuf(stderr, NULL, _IONBF, 0);
-    char n;
-    char in[20], out[20];
+    char n, stringLine[255], line = '0';
+    char input[255], output[255];
+    int i = 0;
+    float localNum, onenumber, twonumber;
     FILE *inFile, *outFile;
-     do{
-        printf("Enter input file name: ");
-        scanf("%s", in);
-        inFile = fopen(in, "r");
-        if (inFile == NULL) {           // проверка на существование файла ввода
-            printf("Error enter again\n");
-            while (inFile == NULL) {
-                printf("Enter input file name: ");
-                scanf("%s", in);
-                inFile = fopen(in, "r");
+    do{
+        printf("Enter input file name:\n");
+        scanf("%s", input);
+        inFile = fopen(input,"r");  //   открыть входной файл
+        if (inFile == NULL){
+            printf("File is empty. Try again.\n");
+            while (inFile == NULL){
+                printf("Enter input file name:\n");
+                scanf(" %s", input);
+                inFile = fopen(input,"r");
             }
         }
-        queue_t queue = init_queue();            // создаём пустую очередь
-        while (!feof(inFile))
-            enqueue(&queue, read_string(inFile)); // добавляем в очередь строки
-        fclose(inFile);
-        printf("Enter output file name: ");
-        scanf("%s", out);
-        outFile = fopen(out, "w");
-        while (!is_empty(queue)) {         // пока не кончится очередь
-            calc(outFile, dequeue(&queue)); // вычисляем выражение
+        printf("Enter output file name:\n");
+        scanf(" %s", output);
+        outFile = fopen(output, "w");  //  открыть выходной файл
+        while (feof(inFile) == 0){
+            fscanf(inFile, "%c", &stringLine[i]);
+            if (stringLine[i] == ' '){
+                if (line == '0'){
+                    localNum = strtof(stringLine, NULL); //  разделите выражение на символы
+                    pushElement(&headStack, localNum);
+                    i = 0;
+                }
+            }
+            else if (stringLine[i] == '*' || stringLine[i] == '+' || stringLine[i] == '-'	|| stringLine[i] == '/' || stringLine[i] == '^'){
+                onenumber = popElement(&headStack);
+                twonumber = popElement(&headStack);
+                pushElement(&headStack, numberFunction(stringLine[i], onenumber, twonumber));
+                fprintf(outFile, "%.3f %c %.3f = %.3f \n", onenumber, stringLine[i], twonumber, numberFunction(stringLine[i], onenumber, twonumber));
+                line = '1';
+            }
+            else if (stringLine[i] == 'n'){
+                while (headStack != NULL){
+                    popElement(&headStack);
+                }
+                fprintf(outFile, "\n");
+            }
+            else{
+                line = '0';
+                i = i + 1;
+            }
         }
+        fclose(inFile);
         fclose(outFile);
         printf("Want to work with new files(input and output)?\n");
-        scanf("%s", &n);
-    }while (n == 'y');
+        scanf(" %c", &n);
+    }
+    while (n == 'y');
     return 0;
 }
 
